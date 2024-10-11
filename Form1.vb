ef83514838139a61fb855b8e29a2cd7dd6bd5aee
Nuhu Kanga Ali <nuhukali233@gmail.com>
2024-10-11 20:37:55 +0000

    Simple Calculator

---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub calculateBtn_Click(sender As Object, e As EventArgs) Handles calculateBtn.Click
+        Dim numValueOne As Integer = Integer.Parse(firstValueBox.Text)
+        Dim numValueTwo As Integer = Integer.Parse(secondValueBox.Text)
+        Dim finalValue As Integer
+        Select Case operatorBox.SelectedItem
+            Case "*"
+                finalValue = numValueOne * numValueTwo
+            Case "/"
+                finalValue = numValueOne / numValueTwo
+            Case "+"
+                finalValue = numValueOne + numValueTwo
+            Case "-"
+                finalValue = numValueOne - numValueTwo
+            Case Else
+                MessageBox.Show("Kindly enter a vaild number")
+        End Select
+        resultDisplayBox.Text = finalValue.ToString()
+    End Sub
+End Class
